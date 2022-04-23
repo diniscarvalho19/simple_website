d1e12b47d83599edf8e952d85653892ff272dc7e
diniscarvalho19 <49685992+diniscarvalho19@users.noreply.github.com>
2022-04-23 23:25:44 +0200

    comitted

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
 'use strict'
-
+for (let i = 0; i < localStorage.length; i++){
+    let key = localStorage.key(i);
+    var li = "<li><a>" + key + "</a></li>";
+    document.getElementById("list").insertAdjacentHTML('afterend', li);
+  }
 const switcher = document.querySelector('.btn');
 
 switcher.addEventListener('click', function() {
     document.body.classList.toggle('dark-theme')
 
     var className = document.body.className;
-    if(className == "dark-theme") {    
+    if(className == "light-theme") {    
         this.textContent = "Dark";
     }
     else {
         this.textContent = "Light";
     }
 
-    console.log('current class name: ' + className);
-
 });
 
 document.getElementById("add").onclick = function() {
-    var text = document.getElementById("input").value; 
-    var li = "<li>" + text + "</li>";
-    document.getElementById("list").insertAdjacentHTML('beforeend', li);
-    document.getElementById("input").value = ""; // clear the value
+    var text = document.getElementById("input").value;
+    if(text == ""){
+    }
+    else{
+        var li = "<li><a>" + text + "</a></li>";
+        document.getElementById("list").insertAdjacentHTML('afterend', li);
+        document.getElementById("input").value = ""; // clear the value
+        localStorage.setItem(text, 'Value'); //add value to local storage        
+    }
   }
+
+  document.getElementById("clear").onclick = function() {
+    localStorage.clear();    //clear list
+    document.getElementById("list").innerHTML = "";
+    }
+  
