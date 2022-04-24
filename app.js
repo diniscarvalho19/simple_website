e6524cd9c08e5c8558e075fdb3935758478b489d
diniscarvalho19 <49685992+diniscarvalho19@users.noreply.github.com>
2022-04-24 14:05:03 +0200

    ranking system

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 'use strict'
 for (let i = 0; i < localStorage.length; i++){
     let key = localStorage.key(i);
-    var li = "<li><a>" + key + "</a></li>";
+    var rating = parseInt(localStorage.getItem(key)) + 1;
+    var li = "<li><a>" + key + " <input type='button' value='Upvote' id='uv' /> <input type='button' value='Downvote' id='dv' /> " + parseInt(localStorage.getItem(key)) + "</a></li>";
     document.getElementById("list").insertAdjacentHTML('afterend', li);
+    document.getElementById("uv").onclick = function() {
+        var rating = parseInt(localStorage.getItem(key)) + 1;
+        localStorage.setItem(key,rating);
+        console.log("UP+ " +  localStorage.getItem(key))
+        window.location.reload();
+    }
+    document.getElementById("dv").onclick = function() {
+        var rating = parseInt(localStorage.getItem(key)) - 1;
+        localStorage.setItem(key, rating);
+        console.log("DV- " +  localStorage.getItem(key))
+        window.location.reload();
+    }  
   }
+
 const switcher = document.querySelector('.btn');
 
 switcher.addEventListener('click', function() {
@@ -24,15 +38,35 @@ document.getElementById("add").onclick = function() {
     if(text == ""){
     }
     else{
-        var li = "<li><a>" + text + "</a></li>";
+        var li = "<li><a>" + text + "  <input type='button' value='Upvote' id='uv' /> <input type='button' value='Downvote' id='dv' />" + parseInt(localStorage.getItem(text)) + "</a></li>";
         document.getElementById("list").insertAdjacentHTML('afterend', li);
         document.getElementById("input").value = ""; // clear the value
-        localStorage.setItem(text, 'Value'); //add value to local storage        
+        localStorage.setItem(text, parseInt(0)); //add value to local storage
+        window.location.reload();  
+        document.getElementById("uv").onclick = function() {
+            var rating = parseInt(localStorage.getItem(text)) + 1;
+            localStorage.setItem(text,rating);
+            console.log("UP+ " +  localStorage.getItem(text))
+            window.location.reload();
+        }
+        document.getElementById("dv").onclick = function() {
+            var rating = parseInt(localStorage.getItem(text)) - 1;
+            localStorage.setItem(text, rating);
+            console.log("DV- " +  localStorage.getItem(text))
+            window.location.reload();
+        }            
     }
   }
 
   document.getElementById("clear").onclick = function() {
     localStorage.clear();    //clear list
     document.getElementById("list").innerHTML = "";
+    window.location.reload();
     }
+
+
+
+
+
+    
   
